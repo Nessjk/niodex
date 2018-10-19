32dde5bf0ebcf7842173f4e23781651526209903
Tony Rositano <tony.rositano@pandauxstudio.com>
2018-10-19 16:10:16 +0200

    Add @front hero section + svg

--- scripts/main.js
+++
@@ -1,7 +0,0 @@
-let file = document.getElementById('file');
-file.addEventListener('change', function() {
-    if(file && file.value) {
-        let val = file.files[0].name;
-        document.getElementById('value').innerHTML = "Selected" + val;
-    }
-});
